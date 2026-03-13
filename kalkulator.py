d8862d4cd769119e6b56eb7d9331bf9190487bae
Alicja <124045087+Alicja53@users.noreply.github.com>
2026-03-13 08:47:58 +0100

    Implement division by zero error handling
    Added error handling for division by zero in kalkulator.py.

--- kalkulator.py
+++ kalkulator.py
@@ -4,8 +4,12 @@ wybor = int(input("Wybierz 1 - dzielenie a / b, wybierz 2 - dzielenie b / a")
 if (wybor != 1 or wybor != 2):
             print("Nie ma takiej opcji") 
 elif(wybor == 1):
+            if(b==0):
+                        print("Błąd dzielenia przez 0")
   wynik = a/b
 else:
+            if(a==0):
+                        print("Błąd dzielenia przez 0")
   wynik = b/a 
 
 print(wynik)
